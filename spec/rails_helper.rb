ab80e665a497d248f45a09d1911e99c2bf4c2adc
RubyBossKirill <160587634+RubyBossKirill@users.noreply.github.com>
2025-01-23 18:19:48 +0500

    feat: spec settings

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] = 'test'
 ENV['APP_HOST'] = 'http://localhost'
 
-require_relative '../config/environment'
+require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |file| require file }
